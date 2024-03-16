88d744112fc3759e8ede5d6f45c2937b5a175623
Ellams George <ellamsgeorge7@gmail.com>
2024-03-16 13:55:52 +0100

    push

--- src/app/feature/page.jsx
+++ src/app/feature/page.jsx
@@ -1,33 +1,31 @@
-import Image from "next/image";
-import items from "./components/Workdata";
-import FAQ from "./components/faq";
-import Pricinglist from "./components/pricing/pricinglist";
-export default function Home() {
+import items from "../components/Workdata";
+
+const Page = () => {
   return (
     <div>
-
- 
-    <main className="  ">
       <div className="lg:px-[86px] px-[30px] pt-5  text-center">
         <div className="leading-tight font-extrabold web-title">
-          <h1 className=" uppercase">From</h1>
           <h1 className=" uppercase">
-            <span> Finding</span> Prospects to Converting Them into{" "}
-            <span>Paying </span> Customers
+            Discover <span>OUr</span> Feature Set
           </h1>
         </div>
         <div className="flex justify-center">
           <div className="md:w-[60%] md:py-0 py-2 paragraphs">
             <p>
-              Empower your business with OutreachAI, the all-in-one solution
-              handling prospect discovery to customer conversion. Say goodbye to
-              outreach complexities; let AI streamline your journey to success.
+              Explore the rich array of features that empower our platform to
+              meet your unique needs. From essential functionalities to advanced
+              capabilities.
             </p>
           </div>
         </div>
+      
       </div>
 
-      <div className="flex lg:flex-row text-center flex-col py-6 md:py-5 justify-between md:px-[100px] px-[30px] gap-[76px]">
+      <div className="flex py-6 lg:px-[86px] px-[30px]  justify-center">
+        <img className="md:w-[70%]" src="image/laptop.webp" alt="screen"/>
+      </div>
+
+      <div className="flex lg:flex-row text-center flex-col py-6 md:py-[100px] justify-between md:px-[100px] px-[30px] gap-[76px]">
         <div>
           <img alt="image1" src="" />
           <p className="md:text-[30px] text-[18px] lights">Real-Time Data</p>
@@ -38,7 +36,9 @@ export default function Home() {
         </div>
         <div>
           <img alt="image1" src="" />
-          <p className="md:text-[30px] text-[18px] lights">Smart User Predictions</p>
+          <p className="md:text-[30px] text-[18px] lights">
+            Smart User Predictions
+          </p>
           <p className="paragraphs">
             Develop AI models that predict the potential success of email
             campaigns for specific user segments.
@@ -53,7 +53,7 @@ export default function Home() {
           </p>
         </div>
       </div>
-
+      
       <div className="lg:px-[40px] px-[17px]">
         <div className="bg-black rounded-[2rem] lg:rounded-[5rem] text-white py-[40px] lg:py-[90px] px-[15px] lg:px-[65px]">
           <p className="web-title2 font-extra-bold pb-4 text-left uppercase">
@@ -108,7 +108,7 @@ export default function Home() {
             </div>
 
             <div className="bg-[#0000ff]  card flex flex-col justify-center text-center">
-              <img src="image/card2.webp" alt="" />
+              <img src="image/card1.webp" alt="" />
               <p className="md:text-[30px] text-[21px] !text-white ">
                 Automated Prospect Discovery
               </p>
@@ -119,15 +119,10 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="w-[100%]">
+          <div>
             <div className="flex md:flex-row py-5 gap-3 flex-col">
-              <div className="bg-white lg:w-[50%] card flex flex-col justify-center text-center">
-               
-               
-            
-                <img className=" " src="image/card3.webp" alt="" />
-                
-                
+              <div className="bg-white card flex flex-col justify-center text-center">
+                <img src="image/card1.webp" alt="" />
                 <p className="md:text-[30px] text-[21px] text-black ">Smart Scheduling</p>
                 <p className="paragraphs">
                   Optimize outreach timing based on prospect time zones,
@@ -135,8 +130,8 @@ export default function Home() {
                 </p>
               </div>
 
-              <div className="bg-white lg:w-[50%] text-white card flex flex-col justify-center text-center">
-                <img src="image/card4.webp" alt="" />
+              <div className="bg-white text-white card flex flex-col justify-center text-center">
+                <img src="image/card1.webp" alt="" />
                 <p className="md:text-[30px] text-[21px] text-black ">A/B Testing</p>
                 <p className="paragraphs">
                   Maximizing response rates by identifying the most effective
@@ -144,8 +139,8 @@ export default function Home() {
                 </p>
               </div>
 
-              <div className="bg-white lg:w-[50%] text-white card flex flex-col justify-center text-center">
-                <img src="image/card5.webp" alt="" />
+              <div className="bg-white text-white card flex flex-col justify-center text-center">
+                <img src="image/card1.webp" alt="" />
                 <p className="md:text-[30px] text-[21px] text-black">Real-time Analytics</p>
                 <p className="paragraphs">
                   Track the success of your campaigns with real-time analytics,
@@ -156,7 +151,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
       <div className=" py-[120px]">
         <div className="flex text-center items-center justify-center ">
           <div className="lg:w-[40%] w-[85%]  ">
@@ -171,47 +165,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      <div className="lg:px-[40px] px-[10px]">
-        <div className="bg-black flex md:flex-row flex-col rounded-[2rem] lg:rounded-[5rem] text-white py-[40px] lg:py-[90px] px-[15px] lg:px-[65px]">
-          <div className="lg:w-[50%]">
-            <div className="lg:w-[70%] ">
-            <p className="web-title2 font-extrabold uppercase leading-tight">
-              frequently <span className="text-orange">asked</span> questions
-            </p>
-            <p className="paragraphs2 md:pt-0 pt-3">
-              Explore our FAQ section to find answers to commonly asked
-              questions about our platform.
-            </p>
-            </div>
-           
-          </div>
-          <div className="lg:w-[50%] md:py-0 py-4">
-          <FAQ/>
-          </div>
-         
-        </div>
-      </div>
-    </main>
-
-    <div>
-    <div className="flex  pb-[70px] pt-[120px] text-center items-center justify-center ">
-        <div className="lg:w-[60%] w-[85%]  ">
-          <p className="web-title2 uppercase font-extrabold ">
-            Flexible Pricing Options
-          </p>
-          <p className="paragraphs">
-            Navigate through our pricing section to discover the ideal
-            subscription plan tailored to your needs.
-          </p>
-        </div>
-      </div>
-      <div className="px-[25px]">
-
-     
-      <Pricinglist/>
-      </div>
-    </div>
     </div>
   );
-}
+};
+
+export default Page;
